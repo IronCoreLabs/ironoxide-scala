dc8b4bb0dc456db801a0b604b32e7fcf0256b169
Craig Colegrove <craig.colegrove@ironcorelabs.com>
2020-03-11 13:57:38 -0600

    switch to tokenize query to be able to check length

--- src/test/scala/ironoxide/v1/FullIntegrationTest.scala
+++ src/test/scala/ironoxide/v1/FullIntegrationTest.scala
@@ -299,8 +299,8 @@ class FullIntegrationTest extends AsyncWordSpec with Matchers with EitherValues 
     "tokenize a string successfully" in {
       val encryptedBlindIndexSalt = sdk.createBlindIndex(validGroupId).unsafeRunSync
       val blindIndexSearch = encryptedBlindIndexSalt.initializeSearch[IO].unsafeRunSync
-      val token1 = blindIndexSearch.tokenizeData("ironcore labs", None)
-      val token2 = blindIndexSearch.tokenizeData("ironcore labs", Some("red"))
+      val token1 = blindIndexSearch.tokenizeQuery("ironcore labs", None)
+      val token2 = blindIndexSearch.tokenizeQuery("ironcore labs", Some("red"))
 
       token1.length shouldBe 8
       token2.length shouldBe 8
