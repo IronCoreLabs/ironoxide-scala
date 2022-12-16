012f5547f5a435eca92bf62a7b377699d9a6055f
Craig Colegrove <craig.colegrove@ironcorelabs.com>
2022-12-16 10:18:46 -0800

    Dependency updates, move to Github Actions

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,5 +1,5 @@
-addSbtPlugin("org.xerial.sbt"    % "sbt-sonatype"           % "2.5")
-addSbtPlugin("com.jsuereth"      % "sbt-pgp"                % "1.1.2")
+addSbtPlugin("org.xerial.sbt"    % "sbt-sonatype"           % "3.9.15")
+addSbtPlugin("com.jsuereth"      % "sbt-pgp"                % "2.1.1")
 addSbtPlugin("com.github.gseitz" % "sbt-release"            % "1.0.13")
-addSbtPlugin("org.scalameta"     % "sbt-scalafmt"           % "2.3.2")
+addSbtPlugin("org.scalameta"     % "sbt-scalafmt"           % "2.5.0")
 addSbtPlugin("org.scalastyle"    %% "scalastyle-sbt-plugin" % "1.0.0")
--- src/main/scala/ironoxide/v1/user/JwtClaims.scala
+++ src/main/scala/ironoxide/v1/user/JwtClaims.scala
@@ -14,21 +14,40 @@ import com.ironcorelabs.{sdk => jsdk}
  */
 case class JwtClaims(
   sub: String,
-  pid: Long,
-  sid: String,
-  kid: Long,
+  pid: Option[Long],
+  prefixedPid: Option[Long],
+  sid: Option[String],
+  prefixedSid: Option[String],
+  kid: Option[Long],
+  prefixedKid: Option[Long],
+  uid: Option[String],
+  prefixedUid: Option[String],
   iat: Long,
   exp: Long
 )
 
 object JwtClaims {
-  def apply(jc: jsdk.JwtClaims): JwtClaims =
+  def apply(jc: jsdk.JwtClaims): JwtClaims = {
+    val pid = if (jc.getPid.isPresent) Some(jc.getPid.getAsLong) else None
+    val prefixedPid = if (jc.getPrefixedPid.isPresent) Some(jc.getPrefixedPid.getAsLong) else None
+    val sid = if (jc.getSid.isPresent) Some(jc.getSid.get) else None
+    val prefixedSid = if (jc.getPrefixedSid.isPresent) Some(jc.getPrefixedSid.get) else None
+    val kid = if (jc.getKid.isPresent) Some(jc.getKid.getAsLong) else None
+    val prefixedKid = if (jc.getPrefixedKid.isPresent) Some(jc.getPrefixedKid.getAsLong) else None
+    val uid = if (jc.getUid.isPresent) Some(jc.getUid.get) else None
+    val prefixedUid = if (jc.getPrefixedUid.isPresent) Some(jc.getPrefixedUid.get) else None
     JwtClaims(
       jc.getSub,
-      jc.getPid,
-      jc.getSid,
-      jc.getKid,
+      pid,
+      prefixedPid,
+      sid,
+      prefixedSid,
+      kid,
+      prefixedKid,
+      uid,
+      prefixedUid,
       jc.getIat,
       jc.getExp
     )
+  }
 }
